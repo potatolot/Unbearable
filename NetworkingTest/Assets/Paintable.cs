da0fa07c3920f652de952abc2ed24ae354d5a320
SmosBois <simondeb1505@gmail.com>
2020-03-14 16:30:59 +0100

    Heb een tekenscene toegevoegd waar spelers kunnen tekenen

---
+++ NetworkingTest/Assets/Paintable.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paintable : MonoBehaviour
+{
+    public GameObject Brush;
+    public float BrushSize = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            //cast a ray to the plane
+            var Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(Ray, out hit))
+            {
+                //instanciate a brush
+                var go = Instantiate(Brush, hit.point + Vector3.up * 0.1f, this.transform.rotation, transform);
+                go.transform.localScale = Vector3.one * BrushSize;
+            }
+
+        }
+    }
+}
--- NetworkingTest/Assets/RelateCounter.cs
+++ NetworkingTest/Assets/RelateCounter.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class RelateCounter : MonoBehaviour
 {
+    public Transform scale;
+    public float Value;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,7 @@ public class RelateCounter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Value = GetComponent<Slider>().value;
+        scale.GetComponent<TextMeshProUGUI>().text = Value.ToString() + " / 10";
     }
 }
